bcc896d25fd6370edc89d055c0479c2037f37c9e
Abdelhamid Ait-ayoub <abdelhamidaitayoub@gmail.com>
2021-12-02 17:13:01 +0100

    🎉 Init

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // class diagram implimentation in java programming language
+        // SOLID Principals
+        // Understand the game
+        // Create Class diagram
+        // Game logic
+    }
+}
